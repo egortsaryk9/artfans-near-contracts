4a0c3c2ad118224231e2e9207b3eeb65a4c28129
egortsaryk9 <egortsaryk@gmail.com>
2022-08-13 02:59:55 +0200

    wip

--- contract/social-network/src/lib.rs
+++ contract/social-network/src/lib.rs
@@ -46,14 +46,16 @@ pub struct AccountLikesStat {
 #[serde(crate = "near_sdk::serde")]
 pub enum ContractCall {
     AddMessage { post_id: PostId, text: String },
-    ToggleLike { post_id: PostId, message_idx: Option<U64> }
+    TogglePostLike { post_id: PostId },
+    ToggleMessageLike { message_id: MessageId },
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub enum ContractCallResult {
     AddMessageResult { message_id: MessageId },
-    ToggleLikeResult { is_liked: bool }
+    TogglePostLikeResult { is_liked: bool },
+    ToggleMessageLikeResult { is_liked: bool }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -82,19 +84,24 @@ impl Contract {
         Self {
             owner,
             fee_ft,
-            posts: LookupMap::new(b'm'),
+            posts: LookupMap::new(b'p'),
             likes: LookupMap::new(b'l'),
         }
     }
 
     pub fn add_message(&mut self, post_id: PostId, text: String) -> Promise {
-        self.validate_add_message_call(&post_id, &text);
-        self.collect_fee_and_execute(ContractCall::AddMessage { post_id, text })
+        self.assert_add_message_call(&post_id, &text);
+        self.collect_fee_and_execute_call(ContractCall::AddMessage { post_id, text })
     }
 
-    pub fn toggle_like(&mut self, post_id: PostId, message_idx: Option<U64>) -> Promise {
-        self.validate_toggle_like_call(&post_id, &message_idx);
-        self.collect_fee_and_execute(ContractCall::ToggleLike { post_id, message_idx })
+    pub fn toggle_post_like(&mut self, post_id: PostId) -> Promise {
+        self.assert_toggle_post_like_call(&post_id);
+        self.collect_fee_and_execute_call(ContractCall::TogglePostLike { post_id })
+    }
+
+    pub fn toggle_message_like(&mut self, message_id: MessageId) -> Promise {
+        self.assert_toggle_message_like_call(&message_id);
+        self.collect_fee_and_execute_call(ContractCall::ToggleMessageLike { message_id })
     }
 
     pub fn get_post_messages(&self, post_id: PostId, from_index: U64, limit: U64) -> Vec<MessageDTO> {
@@ -125,7 +132,7 @@ impl Contract {
 #[near_bindgen]
 impl Contract {
 
-    fn validate_add_message_call(&self, post_id: &PostId, text: &String) {
+    fn assert_add_message_call(&self, post_id: &PostId, text: &String) {
         if post_id.trim().is_empty() {
             env::panic_str("'post_id' is empty or whitespace");
         }
@@ -139,7 +146,7 @@ impl Contract {
 
         let mut post = self.posts.get(&post_id).unwrap_or_else(|| {
             let mut pref = Vec::with_capacity(33);
-            pref.push(b'm');
+            pref.push(b'p');
             pref.extend(env::sha256(post_id.as_bytes()));
             Post {
                 messages: Vector::new(pref),
@@ -162,111 +169,128 @@ impl Contract {
         }
     }
 
-
-    fn validate_toggle_like_call(&self, post_id: &PostId, message_idx: &Option<U64>) {
+    fn assert_toggle_post_like_call(&self, post_id: &PostId) {
         if post_id.trim().is_empty() {
             env::panic_str("'post_id' is empty or whitespace");
         }
-        if let Some(idx) = message_idx {
-            // Message like
-            if let Some(post) = self.posts.get(&post_id) {
-                let max_idx = U64(post.messages.len() - 1);
-                if idx > &max_idx {
-                    env::panic_str("'message_idx' is out of bounds");
-                }
-            }
-        } else {
-            // Post like
-        }
     }
 
-
-    fn execute_toggle_like_call(&mut self, post_id: PostId, message_idx: Option<U64>) -> bool {
+    fn execute_toggle_post_like_call(&mut self, post_id: PostId) -> bool {
         let account = env::signer_account_id();
 
-        let mut account_likes = self.likes.get(&account).unwrap_or_else(|| {
+        let mut likes_stat = self.likes.get(&account).unwrap_or_else(|| {
             // Initialize account likes statistic for this post
-            let mut stats_pref = Vec::with_capacity(33);
-            stats_pref.push(b'l');
-            stats_pref.extend(env::sha256(account.as_bytes()));
+            let mut pref_1 = Vec::with_capacity(33);
+            pref_1.push(b'l');
+            pref_1.extend(env::sha256(account.as_bytes()));
 
-            let mut account_likes = AccountLikesStat {
-                posts: LookupMap::new(stats_pref)
+            let mut likes_stat = AccountLikesStat {
+                posts: LookupMap::new(pref_1)
             };
 
-            let mut liked_messages_pref = Vec::with_capacity(65);
-            liked_messages_pref.push(b'l');
-            liked_messages_pref.extend(env::sha256(account.as_bytes()));
-            liked_messages_pref.extend(env::sha256(post_id.as_bytes()));
+            let mut pref_2 = Vec::with_capacity(65);
+            pref_2.push(b'l');
+            pref_2.extend(env::sha256(account.as_bytes()));
+            pref_2.extend(env::sha256(post_id.as_bytes()));
 
             let liked_post = AccountLikedPostState {
                 is_post_liked: false,
-                liked_messages_idx: LookupSet::new(liked_messages_pref)
+                liked_messages_idx: LookupSet::new(pref_2)
             };
 
-            account_likes.posts.insert(&post_id, &liked_post);
-            account_likes
+            likes_stat.posts.insert(&post_id, &liked_post);
+            likes_stat
         });
 
+        let mut liked_post = likes_stat.posts.get(&post_id).unwrap();
+        let is_liked = !liked_post.is_post_liked;
+        liked_post.is_post_liked = is_liked;
+        likes_stat.posts.insert(&post_id, &liked_post);
+        self.likes.insert(&account, &likes_stat);
 
-        let mut liked_post = account_likes.posts.get(&post_id).unwrap();
-
-        let is_liked : bool;
-        if let Some(idx) = message_idx {
-            // Message like
-            let msg_idx = u64::from(idx);
-            is_liked = !liked_post.liked_messages_idx.contains(&msg_idx);
-            if is_liked {
-                liked_post.liked_messages_idx.remove(&msg_idx);
-            } else {
-                liked_post.liked_messages_idx.insert(&msg_idx);
-            }
-
-            let mut post = self.posts.get(&post_id).unwrap();
-            let mut message = post.messages.get(msg_idx).unwrap();
-
-            if is_liked {
-                message.likes_count += 1;
-            } else {
-                message.likes_count -= 1;
-            }
-
-            post.messages.replace(msg_idx, &message);
-            self.posts.insert(&post_id, &post);
-
+        // TODO: Revise
+        let mut post = self.posts.get(&post_id).unwrap();
+        if is_liked {
+            post.likes_count += 1;
         } else {
-            // Post like
-            is_liked = !liked_post.is_post_liked;
-            liked_post.is_post_liked = is_liked;
-            
-            let mut post = self.posts.get(&post_id).unwrap_or_else(|| {
-                let mut pref = Vec::with_capacity(33);
-                pref.push(b'm');
-                pref.extend(env::sha256(post_id.as_bytes()));
-                Post {
-                    messages: Vector::new(pref),
-                    likes_count: 0
-                }
-            });
-
-            if is_liked {
-                post.likes_count += 1;
-            } else {
-                post.likes_count -= 1;
-            }
-
-            self.posts.insert(&post_id, &post);
+            post.likes_count -= 1;
         }
-
-        account_likes.posts.insert(&post_id, &liked_post);
-        self.likes.insert(&account, &account_likes);
+        self.posts.insert(&post_id, &post);
 
         is_liked
     }
 
 
+    fn assert_toggle_message_like_call(&self, message_id: &MessageId) {
+        let post_id = &message_id.post_id;
+        if post_id.trim().is_empty() {
+            env::panic_str("'post_id' is empty or whitespace");
+        }
 
-    fn collect_fee_and_execute(&mut self, call: ContractCall) -> Promise {
+        let msg_idx = &message_id.message_idx;
+        if let Some(post) = self.posts.get(post_id) {
+            let max_idx = U64(post.messages.len() - 1);
+            if msg_idx > &max_idx {
+                env::panic_str("'message_idx' is out of bounds");
+            }
+        }
+    }
+
+
+    fn execute_toggle_message_like_call(&mut self, message_id: MessageId) -> bool {
+        let account = env::signer_account_id();
+        let post_id = message_id.post_id;
+        let msg_idx = u64::from(message_id.message_idx);
+
+        let mut likes_stat = self.likes.get(&account).unwrap_or_else(|| {
+            // Initialize account likes statistic for this post
+            let mut pref_1 = Vec::with_capacity(33);
+            pref_1.push(b'l');
+            pref_1.extend(env::sha256(account.as_bytes()));
+
+            let mut likes_stat = AccountLikesStat {
+                posts: LookupMap::new(pref_1)
+            };
+
+            let mut pref_2 = Vec::with_capacity(65);
+            pref_2.push(b'l');
+            pref_2.extend(env::sha256(account.as_bytes()));
+            pref_2.extend(env::sha256(post_id.as_bytes()));
+
+            let liked_post = AccountLikedPostState {
+                is_post_liked: false,
+                liked_messages_idx: LookupSet::new(pref_2)
+            };
+
+            likes_stat.posts.insert(&post_id, &liked_post);
+            likes_stat
+        });
+
+        let mut liked_post = likes_stat.posts.get(&post_id).unwrap();
+        let is_liked = !liked_post.liked_messages_idx.contains(&msg_idx);
+        if is_liked {
+            liked_post.liked_messages_idx.remove(&msg_idx);
+        } else {
+            liked_post.liked_messages_idx.insert(&msg_idx);
+        }
+        likes_stat.posts.insert(&post_id, &liked_post);
+        self.likes.insert(&account, &likes_stat);
+
+        // TODO: Revise
+        let mut post = self.posts.get(&post_id).unwrap(); // revise
+        let mut message = post.messages.get(msg_idx).unwrap();
+        if is_liked {
+            message.likes_count += 1;
+        } else {
+            message.likes_count -= 1;
+        }
+        post.messages.replace(msg_idx, &message);
+        self.posts.insert(&post_id, &post);
+
+        is_liked
+    }
+
+    fn collect_fee_and_execute_call(&mut self, call: ContractCall) -> Promise {
         ext_ft::ext(self.fee_ft.clone())
             .with_static_gas(Gas(5*TGAS))
             .ft_collect_fee(U128::from(FIXED_FEE))
@@ -290,9 +314,13 @@ impl Contract {
                         let message_id = self.execute_add_message_call(post_id, text);
                         return ContractCallResult::AddMessageResult { message_id }
                     },
-                    ContractCall::ToggleLike { post_id, message_idx } => {
-                        let is_liked = self.execute_toggle_like_call(post_id, message_idx);
-                        return ContractCallResult::ToggleLikeResult { is_liked }
+                    ContractCall::TogglePostLike { post_id } => {
+                        let is_liked = self.execute_toggle_post_like_call(post_id);
+                        return ContractCallResult::TogglePostLikeResult { is_liked }
+                    },
+                    ContractCall::ToggleMessageLike { message_id } => {
+                        let is_liked = self.execute_toggle_message_like_call(message_id);
+                        return ContractCallResult::ToggleMessageLikeResult { is_liked }
                     },
                 }
             },
