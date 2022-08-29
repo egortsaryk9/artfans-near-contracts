3854a5926f89c65d592a30c7ce484d674a581d39
egortsaryk9 <egortsaryk@gmail.com>
2022-08-30 01:12:40 +0200

    wip

--- contract/marketplace/src/external.rs
+++ contract/marketplace/src/external.rs
@@ -5,10 +5,12 @@ pub const TGAS: u64 = 1_000_000_000_000;
 
 #[ext_contract(ext_ft)]
 trait FungibleToken {
-    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
+    // fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
+    fn mint(&mut self, account_id: AccountId, ft_amount: U128) -> bool;
+    fn burn(&mut self, account_id: AccountId, ft_amount: U128);
 }
 
 #[ext_contract(ext_self)]
 trait ExtSelf {
-    fn on_activity_ft_purchased(&mut self, buyer_id: AccountId, near_amount: u128, activity_ft_amount: u128) -> Promise;
+    fn on_activity_ft_purchased(&mut self, buyer_id: AccountId, near_amount: u128, ft_amount: u128) -> Promise;
 }
--- contract/marketplace/src/lib.rs
+++ contract/marketplace/src/lib.rs
@@ -1,12 +1,14 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, is_promise_success, near_bindgen, log, AccountId, Gas, Promise, PanicOnDefault
+use near_sdk::{env, is_promise_success, promise_result_as_success, near_bindgen, log, AccountId, Gas, Promise, PanicOnDefault
 };
 use near_sdk::json_types::{U128};
 
 pub mod external;
 pub use crate::external::*;
 
-pub const EXCHANGE_RATE: u128 = 100;
+pub const ACTIVITY_FT_EXCHANGE_RATE: u128 = 100;
+pub const ACTIVITY_FT_STORAGE_DEPOSIT_YOCTO: u128 = 1_250_000_000_000_000_000_000;
+
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -17,7 +19,6 @@ pub struct Contract {
     artfans_nft: AccountId,
     artfans_nft_beneficiary: AccountId
 }
-
 
 #[near_bindgen]
 impl Contract {
@@ -46,30 +47,51 @@ impl Contract {
     
     #[payable]
     pub fn buy_activity_ft(&mut self) -> Promise {
-        let buyer_id = env::predecessor_account_id();
         let near_amount = env::attached_deposit();
-        let activity_ft_amount = near_amount.saturating_mul(EXCHANGE_RATE);
-        self.purchase_activity_ft(buyer_id,near_amount, activity_ft_amount)
+        if near_amount < ACTIVITY_FT_STORAGE_DEPOSIT_YOCTO {
+            env::panic_str("Attached deposit must be greater than 0.00125 NEAR");
+        };
+
+        let buyer_id = env::predecessor_account_id();
+        let ft_amount = near_amount.saturating_mul(ACTIVITY_FT_EXCHANGE_RATE);
+        self.purchase_activity_ft(buyer_id, near_amount, ft_amount)
     }
 
-    fn purchase_activity_ft(&mut self, buyer_id: AccountId, near_amount: u128, activity_ft_amount: u128) -> Promise {
+    fn purchase_activity_ft(&mut self, buyer_id: AccountId, near_amount: u128, ft_amount: u128) -> Promise {
         ext_ft::ext(self.activity_ft.clone())
             .with_static_gas(Gas(5*TGAS))
-            .ft_transfer(buyer_id.clone(), U128::from(activity_ft_amount), None)
+            .with_attached_deposit(ACTIVITY_FT_STORAGE_DEPOSIT_YOCTO)
+            .mint(buyer_id.clone(), U128::from(ft_amount))
                 .then(
                     ext_self::ext(env::current_account_id())
                     .with_static_gas(Gas(5*TGAS))
-                    .on_activity_ft_purchased(buyer_id, near_amount, activity_ft_amount)
+                    .on_activity_ft_purchased(buyer_id, near_amount, ft_amount)
                 )
     }
 
     #[private]
-    pub fn on_activity_ft_purchased(&mut self, buyer_id: AccountId, near_amount: u128, activity_ft_amount: u128) -> u128 {
+    pub fn on_activity_ft_purchased(&mut self, buyer_id: AccountId, near_amount: u128, ft_amount: u128) -> u128 {
         if is_promise_success() {
-            Promise::new(self.activity_ft_beneficiary.clone()).transfer(u128::from(near_amount));
-            activity_ft_amount
+            let result = promise_result_as_success().expect("Unexpected promise result");
+            let charge_storage_fee : bool = near_sdk::serde_json::from_slice::<bool>(&result).ok().expect("Unexpected value result from promise");
+
+            if charge_storage_fee {
+                let decreased_near_amount = near_amount.saturating_sub(ACTIVITY_FT_STORAGE_DEPOSIT_YOCTO);
+                Promise::new(self.activity_ft_beneficiary.clone()).transfer(u128::from(decreased_near_amount));
+
+                let ft_amount_to_burn = ACTIVITY_FT_STORAGE_DEPOSIT_YOCTO.saturating_mul(ACTIVITY_FT_EXCHANGE_RATE);
+                ext_ft::ext(self.activity_ft.clone())
+                    .with_static_gas(Gas(5*TGAS))
+                    .with_attached_deposit(1)
+                    .burn(buyer_id, U128(ft_amount_to_burn));
+
+                ft_amount - ft_amount_to_burn
+
+            } else {
+                Promise::new(self.activity_ft_beneficiary.clone()).transfer(u128::from(near_amount));
+                ft_amount
+            }
         } else {
-            // Refund
             Promise::new(buyer_id.clone()).transfer(u128::from(near_amount));
             0
         }
