9b41d55d64f93f3ffd94825a05cdf3a2aa9799b7
egortsaryk9 <egortsaryk@gmail.com>
2022-09-05 23:14:35 +0200

    Approval Management

--- contract/nft/src/lib.rs
+++ contract/nft/src/lib.rs
@@ -6,16 +6,20 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupSet};
 use near_sdk::{
     assert_one_yocto,
-    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+    env, near_bindgen, ext_contract, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue, Gas, Balance
 };
 use near_sdk::json_types::U128;
 use std::collections::HashMap;
+use std::mem::size_of;
 
-
-pub const NFT_MAX_SUPPLY: u128 = 26_000;
+pub const NFT_MAX_SUPPLY: u128 = 50;
 pub const NFT_PRICE: u128 = 3_500_000_000_000_000_000_000_000;
 pub const NFT_REGISTRATION_FEE: u128 = 100_000_000_000_000_000_000_000;
 pub const NFT_MINT_BATCH_LIMIT: u8 = 10;
+
+// const GAS_FOR_NFT_APPROVE: Gas = Gas(10_000_000_000_000);
+const GAS_FOR_NFT_APPROVE: Gas = Gas(20_000_000_000_000);
+
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -108,7 +112,8 @@ impl Contract {
                 );
 
                 if let Some(ref account_id) = approve_receiver_id {
-                    self.tokens.nft_approve(token_id, account_id.clone(), approve_msg.clone());
+                    // self.tokens.nft_approve(token_id, account_id.clone(), approve_msg.clone());
+                    self.gas_safe_internal_approve(token_id, account_id.clone(), approve_msg.clone());
                 };
                 result.push(token);
             }
@@ -208,6 +213,82 @@ impl Contract {
         }
     }
 
+    fn bytes_for_approved_account_id(&self, account_id: &AccountId) -> u64 {
+        // The extra 4 bytes are coming from Borsh serialization to store the length of the string.
+        account_id.as_str().len() as u64 + 4 + size_of::<u64>() as u64
+    }
+
+    fn gas_safe_internal_approve(
+        &mut self,
+        token_id: TokenId,
+        account_id: AccountId,
+        msg: Option<String>,
+    ) {
+        let approvals_by_id = self.tokens.approvals_by_id.as_mut().unwrap_or_else(|| env::panic_str("NFT does not support Approval Management"));
+
+        let owner_id = self.tokens.owner_by_id.get(&token_id).expect("Token not found");
+
+        assert!(env::predecessor_account_id() == owner_id, "Predecessor must be token owner.");
+
+        let next_approval_id_by_id = self.tokens.next_approval_id_by_id.as_mut().expect("next_approval_by_id must be set for approval ext");
+        // update HashMap of approvals for this token
+        let approved_account_ids = &mut approvals_by_id.get(&token_id).unwrap_or_default();
+        let approval_id: u64 = next_approval_id_by_id.get(&token_id).unwrap_or(1u64);
+        let old_approval_id = approved_account_ids.insert(account_id.clone(), approval_id);
+
+        // save updated approvals HashMap to contract's LookupMap
+        approvals_by_id.insert(&token_id, approved_account_ids);
+
+        // increment next_approval_id for this token
+        next_approval_id_by_id.insert(&token_id, &(approval_id + 1));
+
+        // If this approval replaced existing for same account, no storage was used.
+        // Otherwise, require that enough deposit was attached to pay for storage, and refund
+        // excess.
+        let storage_used = if old_approval_id.is_none() { self.bytes_for_approved_account_id(&account_id) } else { 0 };
+        self.refund_deposit(storage_used);
+
+        // if given `msg`, schedule call to `nft_on_approve` and return it. Else, return None.
+        msg.map(|msg| {
+            ext_nft_approval_receiver::ext(account_id)
+                // .with_static_gas(env::prepaid_gas() - GAS_FOR_NFT_APPROVE)
+                .with_static_gas(GAS_FOR_NFT_APPROVE)
+                .nft_on_approve(token_id, owner_id, approval_id, msg)
+        });
+    }
+
+
+    fn refund_deposit_to_account(&self, storage_used: u64, account_id: AccountId) {
+        let required_cost = env::storage_byte_cost() * Balance::from(storage_used);
+        let attached_deposit = env::attached_deposit();
+
+        assert!(
+            required_cost <= attached_deposit,
+            "Must attach {} yoctoNEAR to cover storage", required_cost
+        );
+
+        let refund = attached_deposit - required_cost;
+        if refund > 1 {
+            Promise::new(account_id).transfer(refund);
+        }
+    }
+
+    /// Assumes that the precedecessor will be refunded
+    fn refund_deposit(&self, storage_used: u64) {
+        self.
+        refund_deposit_to_account(storage_used, env::predecessor_account_id())
+    }
+}
+
+#[ext_contract(ext_nft_approval_receiver)]
+pub trait NonFungibleTokenReceiver {
+    fn nft_on_approve(
+        &mut self,
+        token_id: TokenId,
+        owner_id: AccountId,
+        approval_id: u64,
+        msg: String,
+    );
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
